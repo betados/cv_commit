aa475415ee86f9ffa91d0586db61ec4f3c6adfbc
Alvaro Torres <torrestal@gmail.com>
2018-12-10 11:07:03 +0100

    argparser working correctly

--- cvCommit.py
+++ cvCommit.py
@@ -2,16 +2,14 @@ import argparse
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Description')
-	subparsers = parser.add_subparsers(help='sub-command help')
+	subparsers = parser.add_subparsers(help='subparsers help', dest='command')
 
 	parser_commit = subparsers.add_parser('commit', help='commit help')
 	parser_commit.add_argument('-m', help='', dest='message')
 
 	parser_r = subparsers.add_parser('rebase', help='rebase help')
-	parser_r.add_argument('-m', help='', dest='message')
+	parser_r.add_argument('branch', help='')
 
 	args = parser.parse_args()
-	args_c = parser_commit.parse_args()
 
 	print(args)
-	print(args_c)
