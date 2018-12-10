c076bc378e5b350537987fe17d026d4c9460a365
Alvaro Torres <torrestal@gmail.com>
2018-12-10 12:31:12 +0100

    implemented functions for the commands

--- cvCommit.py
+++ cvCommit.py
@@ -1,15 +1,39 @@
 import argparse
+import yaml
+
+
+def init():
+    stream = open('.cv.yaml', 'w')
+    yaml.dump({}, stream)
+
+
+def commit():
+    pass
+
+
+def rebase():
+    pass
+
+
+commands = {
+    'init': init,
+    'commit': commit,
+    'rebase': rebase,
+}
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Description')
-	subparsers = parser.add_subparsers(help='subparsers help', dest='command')
+    parser = argparse.ArgumentParser(description='Description')
+    subparsers = parser.add_subparsers(help='subparsers help', dest='command')
 
-	parser_commit = subparsers.add_parser('commit', help='commit help')
-	parser_commit.add_argument('-m', help='', dest='message')
+    parser_init = subparsers.add_parser('init', help='init help')
 
-	parser_r = subparsers.add_parser('rebase', help='rebase help')
-	parser_r.add_argument('branch', help='')
+    parser_commit = subparsers.add_parser('commit', help='commit help')
+    parser_commit.add_argument('-m', help='', dest='message')
 
-	args = parser.parse_args()
+    parser_rebase = subparsers.add_parser('rebase', help='rebase help')
+    parser_rebase.add_argument('branch', help='')
 
-	print(args)
+    args = parser.parse_args()
+
+    # print(args)
+    commands[args.command]()
