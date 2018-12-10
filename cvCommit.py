b9e4ca24cea34f7ce67c89125e834fa058a562c7
AlvaroTorres <torrestal@gmail.com>
2018-12-10 08:05:57 +0100

    added subparser but not working

--- cvCommit.py
+++ cvCommit.py
@@ -1,11 +1,17 @@
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('commit', help='')
-    parser.add_argument('rebase', help='')
-    parser.add_argument('-m', help='')
+	parser = argparse.ArgumentParser(description='Description')
+	subparsers = parser.add_subparsers(help='sub-command help')
 
-    args = parser.parse_args()
+	parser_commit = subparsers.add_parser('commit', help='commit help')
+	parser_commit.add_argument('-m', help='', dest='message')
 
-    print(args)
+	parser_r = subparsers.add_parser('rebase', help='rebase help')
+	parser_r.add_argument('-m', help='', dest='message')
+
+	args = parser.parse_args()
+	args_c = parser_commit.parse_args()
+
+	print(args)
+	print(args_c)
